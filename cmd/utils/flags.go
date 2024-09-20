19f0873e9899c6480ad1f7017a8e75429524e608
Péter Szilágyi <peterke@gmail.com>
2024-09-20 16:43:42 +0300

    beacon, core, eth, miner: integrate witnesses into production Geth (#30069)
    This PR integrates witness-enabled block production, witness-creating
    payload execution and stateless cross-validation into the `engine` API.
    The purpose of the PR is to enable the following use-cases (for API
    details, please see next section):
    
    - Cross validating locally created blocks:
    - Call `forkchoiceUpdatedWithWitness` instead of `forkchoiceUpdated` to
    trigger witness creation too.
    - Call `getPayload` as before to retrieve the new block and also the
    above created witness.
    - Call `executeStatelessPayload` against another client to
    cross-validate the block.
    
    - Cross validating locally processed blocks:
    - Call `newPayloadWithWitness` instead of `newPayload` to trigger
    witness creation too.
    - Call `executeStatelessPayload` against another client to
    cross-validate the block.
    
    - Block production for stateless clients (local or MEV builders):
    - Call `forkchoiceUpdatedWithWitness` instead of `forkchoiceUpdated` to
    trigger witness creation too.
    - Call `getPayload` as before to retrieve the new block and also the
    above created witness.
    - Propagate witnesses across the consensus libp2p network for stateless
    Ethereum.
    
    - Stateless validator validation:
    - Call `executeStatelessPayload` with the propagated witness to
    statelessly validate the block.
    
    *Note, the various `WithWitness` methods could also *just be* an
    additional boolean flag on the base methods, but this PR wanted to keep
    the methods separate until a final consensus is reached on how to
    integrate in production.*
    
    ---
    
    The following `engine` API types are introduced:
    
    ```go
    // StatelessPayloadStatusV1 is the result of a stateless payload execution.
    type StatelessPayloadStatusV1 struct {
    	Status          string      `json:"status"`
    	StateRoot       common.Hash `json:"stateRoot"`
    	ReceiptsRoot    common.Hash `json:"receiptsRoot"`
    	ValidationError *string     `json:"validationError"`
    }
    ```
    
    - Add `forkchoiceUpdatedWithWitnessV1,2,3` with same params and returns
    as `forkchoiceUpdatedV1,2,3`, but triggering a stateless witness
    building if block production is requested.
    - Extend `getPayloadV2,3` to return `executionPayloadEnvelope` with an
    additional `witness` field of type `bytes` iff created via
    `forkchoiceUpdatedWithWitnessV2,3`.
    - Add `newPayloadWithWitnessV1,2,3,4` with same params and returns as
    `newPayloadV1,2,3,4`, but triggering a stateless witness creation during
    payload execution to allow cross validating it.
    - Extend `payloadStatusV1` with a `witness` field of type `bytes` if
    returned by `newPayloadWithWitnessV1,2,3,4`.
    - Add `executeStatelessPayloadV1,2,3,4` with same base params as
    `newPayloadV1,2,3,4` and one more additional param (`witness`) of type
    `bytes`. The method returns `statelessPayloadStatusV1`, which mirrors
    `payloadStatusV1` but replaces `latestValidHash` with `stateRoot` and
    `receiptRoot`.

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -601,11 +601,6 @@ var (
 		Usage:    "Disables db compaction after import",
 		Category: flags.LoggingCategory,
 	}
-	CollectWitnessFlag = &cli.BoolFlag{
-		Name:     "collectwitness",
-		Usage:    "Enable state witness generation during block execution. Work in progress flag, don't use.",
-		Category: flags.MiscCategory,
-	}
 
 	// MISC settings
 	SyncTargetFlag = &cli.StringFlag{
@@ -1865,9 +1860,6 @@ func SetEthConfig(ctx *cli.Context, stack *node.Node, cfg *ethconfig.Config) {
 		// TODO(fjl): force-enable this in --dev mode
 		cfg.EnablePreimageRecording = ctx.Bool(VMEnableDebugFlag.Name)
 	}
-	if ctx.IsSet(CollectWitnessFlag.Name) {
-		cfg.EnableWitnessCollection = ctx.Bool(CollectWitnessFlag.Name)
-	}
 
 	if ctx.IsSet(RPCGlobalGasCapFlag.Name) {
 		cfg.RPCGasCap = ctx.Uint64(RPCGlobalGasCapFlag.Name)
@@ -2355,7 +2347,6 @@ func MakeChain(ctx *cli.Context, stack *node.Node, readonly bool) (*core.BlockCh
 	}
 	vmcfg := vm.Config{
 		EnablePreimageRecording: ctx.Bool(VMEnableDebugFlag.Name),
-		EnableWitnessCollection: ctx.Bool(CollectWitnessFlag.Name),
 	}
 	if ctx.IsSet(VMTraceFlag.Name) {
 		if name := ctx.String(VMTraceFlag.Name); name != "" {
