f5641aa7ddd8461135f663463689daa3afab0df0
Meng <69021250+limengformal@users.noreply.github.com>
2024-12-03 20:40:41 -0800

    feat(cmd): add OverrideStoryDenomTheogony override option (#68)

--- core/genesis.go
+++ core/genesis.go
@@ -250,8 +250,9 @@ type ChainOverrides struct {
 	OverrideVerkle *uint64
 
 	// For Story
-	Override4844          bool
-	OverrideStoryTheogony *big.Int
+	Override4844               bool
+	OverrideStoryTheogony      *big.Int
+	OverrideStoryDenomTheogony *uint64
 }
 
 // SetupGenesisBlock writes or updates the genesis block in db.
@@ -288,6 +289,9 @@ func SetupGenesisBlockWithOverride(db ethdb.Database, triedb *triedb.Database, g
 			}
 			if overrides != nil && overrides.OverrideStoryTheogony != nil {
 				config.TheogonyBlock = overrides.OverrideStoryTheogony
+			}
+			if overrides != nil && overrides.OverrideStoryDenomTheogony != nil {
+				config.EIP1559DenomTheogony = overrides.OverrideStoryDenomTheogony
 			}
 		}
 	}
