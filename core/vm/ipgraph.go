6c176f13b8217def0b20ff5dfa8fa3f5ddeca336
Kingter <83567446+kingster-will@users.noreply.github.com>
2024-12-09 15:06:38 -0800

    prevent static call write to ipgraph (#66)

--- core/vm/ipgraph.go
+++ core/vm/ipgraph.go
@@ -150,6 +150,11 @@ func (c *ipGraph) addParentIp(input []byte, evm *EVM, ipGraphAddress common.Addr
 	}
 
 	log.Info("addParentIp", "input", input)
+
+	if evm.currentPrecompileCallType != CALL {
+		return nil, fmt.Errorf("addParentIp can only be called with CALL, not %v", evm.currentPrecompileCallType)
+	}
+
 	if len(input) < 96 {
 		return nil, fmt.Errorf("input too short for addParentIp")
 	}
@@ -328,6 +333,10 @@ func (c *ipGraph) setRoyalty(input []byte, evm *EVM, ipGraphAddress common.Addre
 	}
 
 	log.Info("setRoyalty", "ipGraphAddress", ipGraphAddress, "input", input)
+	if evm.currentPrecompileCallType != CALL {
+		return nil, fmt.Errorf("setRoyalty can only be called with CALL, not %v", evm.currentPrecompileCallType)
+	}
+
 	if len(input) < 96 {
 		return nil, fmt.Errorf("input too short for setRoyalty")
 	}
