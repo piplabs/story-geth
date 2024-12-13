032128cf99dae57edf934b3e43ed58ad60b36691
steven <wangpeculiar@gmail.com>
2024-12-13 23:40:18 +0800

    build(deps): bump golang.org/x/crypto from v0.27.0 to v0.31.0 (#79)

--- consensus/misc/eip1559/eip1559.go
+++ consensus/misc/eip1559/eip1559.go
@@ -22,6 +22,7 @@ import (
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/math"
 	"github.com/ethereum/go-ethereum/consensus/misc"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/params"
