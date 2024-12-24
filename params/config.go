ce1dc10b52a585d9812d5992e2de8f74e208859d
0xHansLee <hansol.lee@storyprotocol.xyz>
2024-12-24 20:12:06 +0900

    chore(genesis): update genesis block hash

--- params/config.go
+++ params/config.go
@@ -32,7 +32,7 @@ var (
 	GoerliGenesisHash  = common.HexToHash("0xbf7e331f7f7c1dd2e05159666b3bf8bc7a8a3a9eb1d518969eab529dd9b88c1a")
 	IliadGenesisHash   = common.HexToHash("0xf5ebc6f0982e8bf7fd532b3f959f84d12b3dedd2827af8d31f5389447bedafc6")
 	OdysseyGenesisHash = common.HexToHash("0xf5ebc6f0982e8bf7fd532b3f959f84d12b3dedd2827af8d31f5389447bedafc6")
-	StoryGenesisHash   = common.HexToHash("0xdf5284f83cc659c2731a3e47dc207f5dbc21cd8247ebebd8e19337f7a0798e3f")
+	StoryGenesisHash   = common.HexToHash("0x6c231fccb2b82478faff9976843a77f7246b9cef9861ef6f2d8dfffe37a7cfd6")
 	LocalGenesisHash   = common.HexToHash("0xeeb146844fdd1f8e4fe1af3f12c6169a3a08912633d7873ab59ccaefbd8f426e")
 )
 
