5d80a3cfe46ee677ff861d88ed5ccd848ab8179d
Ze <edisonz0718@gmail.com>
2025-01-30 12:18:58 -0800

    feat(*): remove min priority fee restriction (#98)

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -1855,7 +1855,6 @@ func SetEthConfig(ctx *cli.Context, stack *node.Node, cfg *ethconfig.Config) {
 		}
 		cfg.Genesis = core.DefaultAeneidGenesisBlock()
 		SetDNSDiscoveryDefaults(cfg, params.AeneidGenesisHash)
-		cfg.Miner.GasPrice = big.NewInt(params.GWei * 4)
 		cfg.Miner.GasCeil = 36_000_000
 		cfg.TxPool.NoLocals = true
 	case ctx.Bool(StoryFlag.Name):
@@ -1864,7 +1863,6 @@ func SetEthConfig(ctx *cli.Context, stack *node.Node, cfg *ethconfig.Config) {
 		}
 		cfg.Genesis = core.DefaultStoryGenesisBlock()
 		SetDNSDiscoveryDefaults(cfg, params.StoryGenesisHash)
-		cfg.Miner.GasPrice = big.NewInt(params.GWei * 4)
 		cfg.Miner.GasCeil = 36_000_000
 		cfg.TxPool.NoLocals = true
 	case ctx.Bool(LocalFlag.Name):
